29fc01b8becf5b236e7b6ef14f5cd5ac57347d3a
earthy-zinc <1066365803@qq.com>
2023-11-06 00:35:55 +0800

    修改并测试了去雾系统初步的有效性

--- benchmark/C2PNet/run.py
+++ benchmark/C2PNet/run.py
@@ -3,33 +3,29 @@ from PIL import Image
 import torchvision.transforms as tfs
 import torchvision.utils as torch_utils
 
-from benchmark.C2PNet.C2PNet import C2PNet
+from benchmark.C2PNet.model import C2PNet
 import os
 
 from benchmark.C2PNet.metrics import psnr, ssim
-
-device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-# 获取当前脚本所在目录的绝对路径
-script_dir = os.path.dirname(os.path.abspath(__file__))
+from global_variable import MODEL_PATH, DEVICE
 
 
 def get_model(model_name: str):
     # 构造模型文件的绝对路径
-    model_dir = os.path.join(script_dir, model_name)
-
+    model_dir = os.path.join(MODEL_PATH, model_name)
     net = C2PNet(gps=3, blocks=19)
     ckp = torch.load(model_dir)
-    net = net.to(device)
+    net = net.to(DEVICE)
     net.load_state_dict(ckp['model'])
     net.eval()
     return net
 
 
 def dehaze(haze_image_path: str, output_image_path: str):
-    net = get_model('OTS.pkl')
+    net = get_model('C2PNet/OTS.pkl')
     haze = Image.open(haze_image_path).convert('RGB')
     haze = tfs.ToTensor()(haze)[None, ::]
-    haze = haze.to(device)
+    haze = haze.to(DEVICE)
     with torch.no_grad():
         pred = net(haze)
     ts = torch.squeeze(pred.clamp(0, 1).cpu())
